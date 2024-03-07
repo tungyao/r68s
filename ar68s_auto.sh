45a5a3e7bc94435c8cee4f9c604feeeffebeafb0
tong <1922527051@qq.com>
2024-03-07 16:08:42 +0800

    Update ar68s_auto.sh

--- ar68s_auto.sh
+++ ar68s_auto.sh
@@ -27,8 +27,8 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.con
 ./scripts/feeds install -a
 #修改内核版本
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/rockchip/Makefile
-# 修改默认IP为 192.168.1.180
-sed -i 's/192.168.1.1/192.168.1.180/g' package/base-files/files/bin/config_generate
+# 修改默认IP为 192.168.100.1
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 sed -i "/set network.\$1.proto='static'/{s/$/\n\t\t\t\tset network.\$1.gateway='192.168.1.1'/}" package/base-files/files/bin/config_generate
 sed -i "/set network.\$1.proto='static'/{s/$/\n\t\t\t\tset network.\$1.dns='223.5.5.5 192.168.1.1'/}" package/base-files/files/bin/config_generate
 # 修改默认主机名
